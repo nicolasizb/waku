b7a75e916b024cd4733e551b76176dd4dc9c1ab8
nicolasizb <nicolasizquierdocontacto@gmail.com>
2024-07-13 10:43:01 -0500

    First commit 3

--- src/routes/routes.js
+++ src/routes/routes.js
@@ -7,14 +7,24 @@ router.get('/', (req, res) => {
 })
 
 router.get('/webhook', (req, res) => {
-    res.status(200).json({message: "WEBHOOK"})
-})
+    const VERIFY_TOKEN = 'your_verify_token';
+    const mode = req.query['hub.mode'];
+    const token = req.query['hub.verify_token'];
+    const challenge = req.query['hub.challenge'];
+  
+    if (mode && token) {
+        if (mode === 'subscribe' && token === VERIFY_TOKEN) {
+          console.log('WEBHOOK_VERIFIED');
+          res.status(200).send(challenge);
+        } else {
+          res.sendStatus(403);
+        }
+    }
+});
 
 router.post('/webhook', (req, res) => {
-    const data = req.body
-    console.log("Datos recibidos", data)
-
-    res.status(200).send('Webhook recibido');
-})
+    console.log('POST /webhook received:', req.body);
+    res.sendStatus(200);
+});
 
 module.exports = router
